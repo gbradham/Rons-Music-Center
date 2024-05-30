e867dbe52a7d342cfd17460a20d4c8a5335fd91a
gbradham <gbradham@go.olemiss.edu>
2024-05-30 10:34:37 -0500

    Update composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6e4fdb4824f6360fd8b1eb924987fddd50c1fbff',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6e4fdb4824f6360fd8b1eb924987fddd50c1fbff',
+            'dev_requirement' => false,
+        ),
+    ),
+);
